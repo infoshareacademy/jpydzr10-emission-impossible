37e5cde29110b5903fb2c63fbf79ad63c9b376b7
Adrian Sidorek <adriansidorek@gmail.com>
2026-04-20 22:29:30 +0200

    CREATE: structure django apps

---
+++ web/calculator/utils.py
@@ -0,0 +1,3 @@
+# calculator/utils.py
+
+#TODO Wykorzystanie biblioteki pint do konwersji jednostek
---
+++ web/what_if/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WhatIfConfig(AppConfig):
+    name = 'what_if'
