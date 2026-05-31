ee86e0377db6ba5e48ecc245a542ba08a704fe00
Adrian Sidorek <adriansidorek@gmail.com>
2026-05-31 22:51:14 +0200

    MODIFY: companies views for tailwind, update companies_list

--- web/companies/views.py
+++ web/companies/views.py
@@ -1,67 +1,81 @@
-from django.contrib.auth.decorators import login_required
+from accounts.models import CustomUser
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
 
 from companies.forms import CompaniesForm
 from companies.models import Companies
-from accounts.models import CustomUser
+
 
 class CompanyAccessMixin:
     """
     Mixin filtrujący firmy na podstawie uprawnień zalogowanego użytkownika.
     """
-    required_permission = 'read'
+
+    required_permission = "read"
 
     def get_queryset(self):
         user: CustomUser = self.request.user
 
-        if user.role == 'admin' or user.is_superuser:
+        if user.role == "admin" or user.is_superuser:
             return Companies.objects.all()
 
-        if self.required_permission == 'save':
+        if self.required_permission == "save":
             return Companies.objects.filter(
-                user_permissions__user=user,
-                user_permissions__can_save=True
+                user_permissions__user=user, user_permissions__can_save=True
             ).distinct()
         else:
             return Companies.objects.filter(
-                user_permissions__user=user,
-                user_permissions__can_read=True
+                user_permissions__user=user, user_permissions__can_read=True
             ).distinct()
 
 
 class CompaniesListView(CompanyAccessMixin, ListView):
     model = Companies
-    template_name = 'companies/companies_list.html'
-    context_object_name = 'companies'
+    template_name = "companies/companies_list.html"
+    context_object_name = "companies"
 
 
 class CompaniesDetailView(CompanyAccessMixin, DetailView):
     model = Companies
-    template_name = 'companies/companies_detail.html'
-    pk_url_kwarg = 'pk'
-    context_object_name = 'company'
+    template_name = "companies/companies_detail.html"
+    pk_url_kwarg = "pk"
+    context_object_name = "company"
 
 
 class CompaniesCreateView(LoginRequiredMixin, CreateView):
     model = Companies
     form_class = CompaniesForm
-    template_name = 'companies/companies_detail.html'
-    required_permission = 'save'
-    success_url = '/companies/'
+    template_name = "companies/companies_form.html"
+    required_permission = "save"
+    success_url = reverse_lazy("companies-list")
+
+    def form_valid(self, form):
+        messages.success(self.request, "Pomyślnie dodano nową spółkę.")
+        return super().form_valid(form)
 
 
-class CompaniesUpdateView(LoginRequiredMixin, CreateView):
+class CompaniesUpdateView(LoginRequiredMixin, UpdateView):
     model = Companies
-    pk_url_kwarg = 'pk'
+    pk_url_kwarg = "pk"
     form_class = CompaniesForm
-    template_name = 'companies/companies_detail.html'
-    required_permission = 'save'
+    template_name = "companies/companies_detail.html"
+    required_permission = "save"
+
+    def form_valid(self, form):
+        messages.success(self.request, "Zmiany zostały zapisane.")
+        return super().form_valid(form)
 
 
-class CompaniesDeleteView(LoginRequiredMixin, CreateView):
+class CompaniesDeleteView(LoginRequiredMixin, DeleteView):
     model = Companies
-    pk_url_kwarg = 'pk'
+    pk_url_kwarg = "pk"
     form_class = CompaniesForm
-    template_name = 'companies/companies_detail.html'
+    template_name = "companies/companies_detail.html"
