c882c09e00d24f386dfc19ef88a908f8015b8560
Paulina <oglozap@gmail.com>
2026-06-08 20:46:44 +0200

    feat: add home panel with tiles grid

--- web/config/urls.py
+++ web/config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from core.views import HomeView
 
 urlpatterns = [
+    path("", HomeView.as_view(), name="home"),
     path("admin/", admin.site.urls),
     path("companies/", include("companies.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
